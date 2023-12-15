2398aed190a78dc1dffade60a7e7616117b1bbb2
RafGeorges <raf.georges@gmail.com>
2023-12-15 16:09:57 +0100

    test push image

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,4 +1,4 @@
-import intro_logo from 'public/assets/logo_placeholder.png'
+import intro_logo from '../public/assets/logo_placeholder.png'
 
 
 export default function Intro(){
@@ -7,7 +7,7 @@ export default function Intro(){
                <h1>Rafaël Georges</h1>
                <p>Développeur web</p> 
                <p>& designer graphique</p>
-               <a href="https://drive.google.com/file/d/1lGOEBgx1LaGDyIRW0Cg3-X7a-YPMDoYX/view?usp=drive_link" target="_blank" alt='intro_img'><button>Voir CV</button></a>
+               <a href="https://drive.google.com/file/d/1lGOEBgx1LaGDyIRW0Cg3-X7a-YPMDoYX/view?usp=drive_link" target="_blank" alt='intro_img' ><button>Voir CV</button></a>
             </div>
             <img src={intro_logo} alt='placeholder'/>  
             </section>
