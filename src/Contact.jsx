dd461d4b765a72f2b9f65e18d177499c7e45a3dd
RafGeorges <raf.georges@gmail.com>
2024-01-11 11:31:46 +0100

    ss

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact(){
     return <div className="contact_container" id="contact_container">
                 <div className="contact_column">
                     <h2>CONTACT</h2>
-                    <p>Prenez le temps de m'écrire un petit mail !!</p>
+                    <p>Prenez le temps de m'écrire un petit mail !</p>
                     <a href= "mailto:raf.georges.dev@gmail.com"><button >Écrire</button> </a>  
                 </div>
                 <img src={contact_clock} alt='contact_clock' />
